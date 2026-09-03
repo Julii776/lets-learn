e955e22f1b0171632298c33357117a6bec577e46
Juli Sah <juli@truemark.tech>
2026-09-03 13:13:05 +0545

    chores: add ui library, react-hook-form, react query

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { NextIntlClientProvider } from 'next-intl';
+import { Toaster } from 'react-hot-toast';
+
 import '../globals.css';
+
+import { QueryProvider } from '@/providers/query-provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +29,10 @@ export default function RootLayout({ children }: LayoutProps<'/[locale]'>) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        <NextIntlClientProvider>
+          <QueryProvider>{children}</QueryProvider>
+          <Toaster />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
---
+++ providers/query-provider.tsx
@@ -0,0 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { PropsWithChildren } from 'react';
+
+export const QueryProvider = ({ children }: PropsWithChildren) => {
+  const client = new QueryClient({
+    defaultOptions: {},
+  });
+
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+};
